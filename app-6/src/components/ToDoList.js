a59d143ff415fdd9fe9a36036ed62ef81958e324
Daniel Cope <danielcope@users.noreply.github.com>
2021-03-03 21:48:01 -0800

    made some progress

---
+++ app-6/src/components/ToDoList.js
@@ -0,0 +1,26 @@
+import React, { Componant, Component } from 'react'
+
+
+
+class ToDoList extends Component {
+
+  constructor () {
+    
+  }
+
+
+
+  render() {
+    return (
+      <section>
+        <header>To Do ToDoList</header>
+        <input />
+        <button> Add </button>
+        
+
+      </section>
+    )
+  }
+}
+
+export default ToDoList
